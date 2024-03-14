047b43424b5ed2a721d23ef8b06df8d433e9757c
Thi Thach <juliet.thi.thach@gmail.com>
2024-03-13 21:26:38 -0400

    remove starter code

--- src/App.js
+++ src/App.js
@@ -1,26 +1,13 @@
 import './App.css';
-import DashBoardPage from './Pages/DashboardPage/DashboardPage';
-import ProfilePage from './Pages/ProfilePage/ProfilePage';
-import PublicProfilePage from './Pages/PublicProfilePage/PublicProfilePage';
+// import Header from '../src/components/Header';
+import DashBoardPage from './pages/DashboardPage/DashboardPage';
+import ProfilePage from './pages/ProfilePage/ProfilePage';
+import PublicProfilePage from './pages/PublicProfilePage/PublicProfilePage';
 import {BrowserRouter, Routes, Route} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<DashBoardPage/>}/>
---
+++ src/pages/DashboardPage/DashboardPage.js
@@ -0,0 +1,9 @@
+import "./DashboardPage.scss";
+
+export default function DashboardPage() {
+  return (
+    <div className="main">
+      <h1>hello world! :3</h1>
+    </div>
+  );
+}
